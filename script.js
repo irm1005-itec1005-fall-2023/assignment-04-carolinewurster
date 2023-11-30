ed7cfbee5b2c19eed5867439b679b6eeb00f6db7
carolinewurster <144635890+carolinewurster@users.noreply.github.com>
2023-11-29 23:14:24 -0500

    Javascript

--- script.js
+++ script.js
@@ -1,43 +1,134 @@
-/* Assignment 04: Finishing a Todo List App
- *
- * 
- *
- */
+const addButton = document.getElementById('addTaskButton');
+const taskInput = document.getElementById('taskInput');
+const tasksContainer = document.getElementById('tasksContainer');
+const completedTasksContainer = document.getElementById('completedTasks');
+const emptyState = document.getElementById('emptyState');
 
+function createEditableTask(taskText) {
+  const newTask = document.createElement("div");
+  newTask.classList.add("task");
 
-//
-// Variables
-//
+  const taskContent = document.createElement("span");
+  taskContent.textContent = taskText;
+  taskContent.classList.add("task-content");
+  taskContent.contentEditable = true;
 
-// Constants
-const appID = "app";
-const headingText = "To do. To done. ✅";
+  const checkmarkButton = document.createElement("button");
+  checkmarkButton.innerHTML = "✓";
+  checkmarkButton.classList.add("checkmark-btn");
 
-// DOM Elements
-let appContainer = document.getElementById(appID);
+  const deleteButton = document.createElement("button");
+  deleteButton.innerHTML = "❌";
+  deleteButton.classList.add("delete-btn");
 
-//
-// Functions
-//
+  deleteButton.onclick = function () {
+    if (newTask.parentElement) {
+      newTask.parentElement.removeChild(newTask);
+      saveTasks();
+    }
+  };
 
-// Add a heading to the app container
-function inititialise() {
-  // If anything is wrong with the app container then end
-  if (!appContainer) {
-    console.error("Error: Could not find app contianer");
+  newTask.appendChild(taskContent);
+  newTask.appendChild(checkmarkButton);
+  newTask.appendChild(deleteButton);
+
+  checkmarkButton.onclick = function () {
+    newTask.classList.toggle("completed");
+    if (newTask.classList.contains("completed")) {
+      tasksContainer.removeChild(newTask);
+      completedTasksContainer.appendChild(newTask);
+      newTask.removeChild(checkmarkButton);
+      taskContent.contentEditable = false;
+    }
+    saveTasks();
+    toggleEmptyState();
+  };
+
+  taskContent.addEventListener('input', () => {
+    saveTasks();
+  });
+
+  return newTask;
+}
+
+function moveTaskToCompleted(taskElement) {
+  taskElement.classList.toggle("completed");
+  tasksContainer.removeChild(taskElement);
+  completedTasksContainer.appendChild(taskElement);
+  taskElement.removeChild(taskElement.querySelector(".checkmark-btn"));
+  taskElement.querySelector(".task-content").contentEditable = false;
+}
+
+function toggleEmptyState() {
+  if (tasksContainer.children.length > 0 || completedTasksContainer.children.length > 0) {
+    emptyState.style.display = "none";
+  } else {
+    emptyState.style.display = "block";
+  }
+}
+
+addButton.addEventListener('click', () => {
+  const taskText = taskInput.value.trim();
+  if (taskText === "") {
+    alert("Please enter a task!");
     return;
   }
 
-  // Create an h1 and add it to our app
-  const h1 = document.createElement("h1");
-  h1.innerText = headingText;
-  appContainer.appendChild(h1);
+  const newTask = createEditableTask(taskText);
+  tasksContainer.appendChild(newTask);
+  taskInput.value = "";
 
-  // Init complete
-  console.log("App successfully initialised");
+  saveTasks();
+  toggleEmptyState();
+});
+
+function saveTasks() {
+  localStorage.setItem('tasks', tasksContainer.innerHTML);
+  localStorage.setItem('completedTasks', completedTasksContainer.innerHTML);
 }
 
-//
-// Inits & Event Listeners
-//
-inititialise();
+window.addEventListener('DOMContentLoaded', () => {
+  if (localStorage.getItem('tasks')) {
+    tasksContainer.innerHTML = localStorage.getItem('tasks');
+    attachEventListenersToTasks(tasksContainer);
+  }
+  if (localStorage.getItem('completedTasks')) {
+    completedTasksContainer.innerHTML = localStorage.getItem('completedTasks');
+    attachEventListenersToTasks(completedTasksContainer);
+  }
+  toggleEmptyState();
+});
+
+function attachEventListenersToTasks(container) {
+  const deleteButtons = container.querySelectorAll('.delete-btn');
+  const checkmarkButtons = container.querySelectorAll('.checkmark-btn');
+
+  deleteButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+      const task = button.closest('.task');
+      if (task.parentElement) {
+        task.parentElement.removeChild(task);
+        saveTasks();
+        toggleEmptyState();
+      }
+    });
+  });
+
+  checkmarkButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+      const task = button.closest('.task');
+      moveTaskToCompleted(task);
+      saveTasks();
+      toggleEmptyState();
+    });
+  });
+}
+
+tasksContainer.addEventListener('click', (event) => {
+  const clickedElement = event.target;
+  if (clickedElement.classList.contains('checkmark-btn')) {
+    const task = clickedElement.closest('.task');
+    moveTaskToCompleted(task);
+    toggleEmptyState();
+  }
+});
